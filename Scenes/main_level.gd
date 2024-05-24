81bcefe07d79dfb9008a21727ebde78b6ccd52ad
Salvatore <salvavel_development@outlook.de>
2024-05-24 21:16:07 +0200

    test commit

--- Scenes/main_level.gd
+++ Scenes/main_level.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	sprite_2d.modulate = Color.RED
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
