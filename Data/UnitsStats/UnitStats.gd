d7c4140ed3ac77f6db498e5bc7e4d15545c6b89f
Salvatore <salvavel_development@outlook.de>
2024-05-30 10:21:17 +0200

    Add custom resource "UnitStats". Add simnple start menu

---
+++ Data/UnitsStats/UnitStats.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name UnitStats
+
+@export var health: float
+@export var speed: float
+@export var offense: float
+@export var defense: float
+@export var icon: CompressedTexture2D
