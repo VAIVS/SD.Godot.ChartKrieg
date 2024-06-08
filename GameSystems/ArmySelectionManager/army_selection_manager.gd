e438d191517dfaaa70857617685b225fc9f3e0fb
Salvatore <salvavel_development@outlook.de>
2024-06-08 21:02:29 +0200

    Ass rudimentary event resolving

--- GameSystems/ArmySelectionManager/army_selection_manager.gd
+++ GameSystems/ArmySelectionManager/army_selection_manager.gd
@@ -8,19 +8,22 @@ var cursor_highlits_dic = {
 	
 }
 
+func get_selection() -> Array:
+	return cursor_highlits_dic.keys()
+
 func handle_signal_on_army_selected(army: TestPlayer):
 	#In future, further modularize this
 	var army_selection: Array = cursor_highlits_dic.keys()
 	if army in army_selection:
 		cursor_highlits_dic[army].queue_free()
 		cursor_highlits_dic.erase(army)
-		game_state_manager.queue_game_state_event(GameStateManager.GameStateEvent.new(GameStateManager.game_event_types.UNSELECTED, army))
+		game_state_manager.queue_game_state_event(GameStateManager.GameStateEvent.new(GameStateManager.game_event_types.UNSELECTED_SINGLE_ARMY, army))
 	else:
 		var army_highligt = army_highlight_scene.instantiate()
 		army.add_child(army_highligt)
 		cursor_highlits_dic[army] = army_highligt
-		game_state_manager.queue_game_state_event(GameStateManager.GameStateEvent.new(GameStateManager.game_event_types.SELECTED, army))
-		
-		
-	
-	
+		var updated_army_selection = cursor_highlits_dic.keys()
+		if updated_army_selection.size() > 1:
+			game_state_manager.queue_game_state_event(GameStateManager.GameStateEvent.new(GameStateManager.game_event_types.SELECTED_MULTIPLE_ARMIES, updated_army_selection))
+		else:
+			game_state_manager.queue_game_state_event(GameStateManager.GameStateEvent.new(GameStateManager.game_event_types.SELECTED_SINGLE_ARMY, army))
--- Scenes/salva/salva_test.gd
+++ Scenes/salva/salva_test.gd
@@ -1,9 +1,24 @@
 extends Node2D
+class_name GameStateManager
+
+enum game_states {
+	MAPIDLE,
+	ARMY_ACTIONS,
+	ARMY_MENU
+}
+
+enum game_event_types {
+	SELECTED_SINGLE_ARMY,
+	SELECTED_MULTIPLE_ARMIES,
+	UNSELECTED_SINGLE_ARMY,
+	CLICKED_ON_MAP
+}
 
 @onready var world_map: WorldMapLogic = $WorldMap
 @onready var armies: Node2D = $Armies
 @onready var army_selection_manager: SelectionManager = $Systens/ArmySelectionManager
-
+@onready var queue: Array[GameStateEvent] = [];
+@onready var current_state: game_states = game_states.MAPIDLE
 
 
 @export var movement_marker_scene: PackedScene
@@ -13,10 +28,13 @@ func _ready():
 	
 	#For the time being done manually. 
 	#In the future this will be done by the system that spawns the armies.
-	
 	for army: TestPlayer in armies.get_children():
 		army.sig_army_selected.connect(army_selection_manager.handle_signal_on_army_selected)
-	
+
+func _process(_delta: float) -> void:
+	var gameEvent: GameStateEvent = queue.pop_front()
+	if gameEvent:
+		process_game_event(gameEvent)
 
 #Todo Move the handling of map controls into the WorlMap
 func set_new_marker(marker: MovementMarker) -> void:
@@ -35,18 +53,42 @@ func _on_map_ready() -> void:
 
 
 
-#func give_move_command(pos: Vector2) -> void:
-	#var marker : MovementMarker = movement_marker_scene.instantiate() as MovementMarker
-	#marker.global_position = pos
-	#for army: TestPlayer in army_selection:
-		#army.target_node = marker
-		## Consider have start_navigation trigger within the setter of target_node.
-		## Depends from navigation map baking.
-		#army.start_navigation()
-		#army.reset_selected_ui()
-	#set_new_marker(marker)
+	
 
 
 func _on_world_map_sig_point_touched(pos: Vector2) -> void:
-	print(pos)
+	queue_game_state_event(GameStateEvent.new(game_event_types.CLICKED_ON_MAP, pos))
+	
+
+func queue_game_state_event(queueItem: GameStateEvent):
+	queue.append(queueItem)
+
+func process_game_event(game_event: GameStateEvent) -> void:
+	if current_state == game_states.MAPIDLE:
+		if game_event.game_event_type == game_event_types.SELECTED_SINGLE_ARMY:
+			pass 
+		elif game_event.game_event_type == game_event_types.SELECTED_MULTIPLE_ARMIES:
+			pass 
+		elif game_event.game_event_type == game_event_types.UNSELECTED_SINGLE_ARMY:
+			pass 
+		elif game_event.game_event_type == game_event_types.CLICKED_ON_MAP:
+			resolve_mapidle_clicked_on_map(game_event.object)
+
+func resolve_mapidle_clicked_on_map(pos: Vector2) -> void:
+	var selection : Array = army_selection_manager.get_selection()
+	var marker : MovementMarker = movement_marker_scene.instantiate() as MovementMarker
+	marker.global_position = pos
+	for army: TestPlayer in selection:
+		army.target_node = marker
+		army.start_navigation()
+	set_new_marker(marker)
+
+class GameStateEvent:
+	
+	var game_event_type: game_event_types
+	var object
+	
+	func _init(et: game_event_types, o):
+		game_event_type = et
+		object = o
 
